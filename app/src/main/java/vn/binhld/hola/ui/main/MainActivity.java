9648af68d9e47c98eb7192f55bee28fee495af6b
binhld <binh.ld172971@sis.hust.edu.vn>
2021-03-24 20:37:20 +0700

    initialize simple mvvm structure

--- app/src/main/java/vn/binhld/hola/ui/main/MainActivity.java
+++ app/src/main/java/vn/binhld/hola/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package vn.binhld.hola;
+package vn.binhld.hola.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import vn.binhld.hola.R;
 
 public class MainActivity extends AppCompatActivity {
 
