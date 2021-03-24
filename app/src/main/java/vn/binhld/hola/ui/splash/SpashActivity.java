b061197f63778f76c37991b79ea7e1a7ce1724c2
binhld <binh.ld172971@sis.hust.edu.vn>
2021-03-24 22:18:16 +0700

    add spash function

---
+++ app/src/main/java/vn/binhld/hola/ui/splash/SpashActivity.java
@@ -0,0 +1,42 @@
+package vn.binhld.hola.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import vn.binhld.hola.R;
+import vn.binhld.hola.ui.main.MainActivity;
+
+public class SpashActivity extends AppCompatActivity {
+    private final int SPLASH_DURATION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(SPLASH_DURATION);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                startMainActivity();
+            }
+        });
+        thread.start();
+    }
+
+    public void startMainActivity() {
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+        finish();
+    }
+
+    public void startLoginActivity() {
+
+    }
+}
---
+++ app/src/main/java/vn/binhld/hola/ui/splash/SpashViewModel.java
@@ -0,0 +1,7 @@
+package vn.binhld.hola.ui.splash;
+
+import androidx.lifecycle.ViewModel;
+
+public class SpashViewModel extends ViewModel {
+
+}
