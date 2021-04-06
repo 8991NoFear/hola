7d12d571ab5d4422cbf0eeb56cdfd8f691d5e022
binhld <binh.ld172971@sis.hust.edu.vn>
2021-04-06 19:27:26 +0700

    set up swipe view for main activity

--- app/src/main/java/vn/binhld/hola/ui/main/MainActivity.java
+++ app/src/main/java/vn/binhld/hola/ui/main/MainActivity.java
@@ -1,17 +1,31 @@
 package vn.binhld.hola.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.tabs.TabLayout;
+
 import vn.binhld.hola.R;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout = null;
+    ViewPager viewPager = null;
+    MyAdapter myAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager); // rang buoc tabLayout voi viewPager
+
+        myAdapter = new MyAdapter(getSupportFragmentManager()); // can co FragmentManager moi tao duoc MyAdapter
+        viewPager.setAdapter(myAdapter); // rang buoc viewPager voi myAdapter
     }
 }
---
+++ app/src/main/java/vn/binhld/hola/ui/main/MyAdapter.java
@@ -0,0 +1,49 @@
+package vn.binhld.hola.ui.main;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+class MyAdapter extends FragmentStatePagerAdapter {
+    public MyAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    // tra ve bien tham chieu toi fragment o vi tri position
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new HomeTab();
+            case 1:
+                return new FriendTab();
+            case 2:
+                return new GroupTab();
+        }
+        return null;
+    }
+
+    // so luong fragment hien co
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    // noi dung hien thi tren fragment o vi tri position
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Home";
+            case 1:
+                return "Friend";
+            case 2:
+                return "Group";
+        }
+        return null;
+    }
+}
